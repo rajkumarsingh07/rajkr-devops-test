d9eda9dd54749d15051bf0d4c046734ada78db03
Singh <singh.rajkumar@siemens.com>
2025-03-17 19:24:04 +0530

    fixed error

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "467.devops.candidate.exam"
-    key    = "rajkumar.singh1" # Replace with your first and last name
+    key    = "rajkumar.singh" # Replace with your first and last name
     region = "ap-south-1"
   }
 }
